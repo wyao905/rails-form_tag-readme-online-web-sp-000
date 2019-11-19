851a9b417751a205076a8e087b1e60269b69c241
Warren Yao <warrenyca905@gmail.com>
2019-11-19 17:31:02 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,5 @@ class PostsController < ApplicationController
   def create
     Post.create(title: params[:post][:title], description: params[:post][:description])
     redirect_to posts_path
+  end
 end
